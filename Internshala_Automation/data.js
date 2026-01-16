94f2431c7eb9354c880e62d2adb8b08f9a0dcd63
Yogeswar Reddy <kamireddyyogi02@gmail.com>
2026-01-16 20:43:46 +0530

    Initial commit

---
+++ Internshala_Automation/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    { 
+      "CareerObjective": "A highly motivated and results-driven Computer Science professional with a strong foundation in full-stack development, web technologies, and problem-solving. Seeking a challenging role in a dynamic and innovative organization where I can leverage my technical expertise in React.js, Node.js.",
+      "College": "India Institute of information technology Pune",
+      "Degree": "Bachelor of Technology (B.Tech)",
+      "Stream": "Computer Science",
+      "Percentage": "7.50",
+      "Training": "Data Structures and Algorithms, Web Development",
+      "Organization": "NA..",
+      "description": "Proficient software developer with a solid foundation in Data Structures, Algorithms, MERN stack, and Java. Seeking an opportunity to leverage my technical expertise to build efficient, scalable applications while solving complex problems.",
+      "ProjectName": "Peronalised Trip-Planner",
+      "ProjectName_Description":"Enabled users to generate custom trip plans by filtering recommendations based on preferences such as budget,weather, and distance.",
+      "link": "https://github.com/",
+      "hiringReason": "Over the past few years, I have acquired relevant skills and experience, which I shall bring to your organization. I have also worked tirelessly on my communication abilities and teamwork skills, which I will put to use in my future career, which would be in your organization if I am selected for the position.",
+      "availability": "Yes, I will be available",
+      "CoverLetter":"I am a computer science student with a strong foundation in full-stack development, particularly using ReactJS for full stack development. Over the course of my academic journey, I have gained hands-on experience by building web applications like a MediLink and a social media app, which helped me sharpen my skills in creating interactive, user-friendly interfaces and efficient back-end systems.",
+      "ExtraQuestion": "I believe I can bring strong value to your team due to my technical expertise, problem-solving skills, and drive for continuous improvement."
+    },
+  ];
+  
